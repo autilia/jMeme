2cdbcc0e371de896ec966346a36daebfeccfe222
Autilia Vitiello <Autilia Vitiello@DESKTOP-LRGE0H5>
2018-12-05 11:56:36 +0100

    Corrected Comment

--- jMeme/src/main/java/jMeme/localSearchAlgorithms/LocalOptimizationAlgorithm.java
+++ jMeme/src/main/java/jMeme/localSearchAlgorithms/LocalOptimizationAlgorithm.java
@@ -123,7 +123,7 @@ public class LocalOptimizationAlgorithm extends Algorithm{
 			solution=(Individual)localOptimizer.getBestSolution();
 			
 			performance.setIndividual(solution);
-			//add in version jMeme 1.0.1
+			//added in the version jMeme 1.0.1
 			performance.computeProblemPerformance(problem, solution);
 			
 			
